aa53520fdc2beba607ebbd8a4140dad08e79bec0
SiPyWiFi <simonwittl@googlemail.com>
2020-07-11 11:40:41 +0200

    Bug fix

--- Converter/ExampleGenerator.py
+++ Converter/ExampleGenerator.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 def return_example_volume():
+    # 3d volume to test
     width, height, depth = 101, 121, 89
     dimensions = (width, height, depth)
 
     array = np.ones(dimensions)*10
-    array[:width//2, :, :] = 5
-    array[width//10:-width//10, height//10:-height//10, depth//10:-depth//10] = 15
+    array[10:20, 10:30, 10:20] = np.ones((10, 20, 10)) * 20
 
     return array
 
--- Converter/main.py
+++ Converter/main.py
@@ -13,28 +13,27 @@ def main():
     image_type = itk.Image[itk.SS, 3]
     ImageCast = itk.CastImageFilter[image_type_input, image_type].New()
     ImageCast.SetInput(example_itk_pointer)
-    ImageCast.Update()
 
     # Extract threshold volume from image
     ThresholdFilter = itk.BinaryThresholdImageFilter[image_type, image_type].New()
     ThresholdFilter.SetInput(ImageCast.GetOutput())
-    ThresholdFilter.SetLowerThreshold(0)
-    ThresholdFilter.SetUpperThreshold(100)
-    ThresholdFilter.SetOutsideValue(0)
-    ThresholdFilter.Update()
+    ThresholdFilter.SetLowerThreshold(19)
+    ThresholdFilter.SetUpperThreshold(21)
+    ThresholdFilter.SetOutsideValue(False)
+    ThresholdFilter.SetInsideValue(True)
 
     # Convert to mesh
-    mesh_type = itk.Mesh[itk.F, 3]
+    mesh_type = itk.Mesh[itk.D, 3]
     MeshFilter = itk.BinaryMask3DMeshSource[image_type, mesh_type].New()
     MeshFilter.SetInput(ThresholdFilter.GetOutput())
-    MeshFilter.SetObjectValue(255)
-    MeshFilter.Update()
+    MeshFilter.SetObjectValue(1)
 
     # Write file to hdd
     Writer = itk.MeshFileWriter[mesh_type].New()
     Writer.SetFileName("Test.obj")
     Writer.SetInput(MeshFilter.GetOutput())
     Writer.Update()
+
 
 if __name__ == "__main__":
     main()
